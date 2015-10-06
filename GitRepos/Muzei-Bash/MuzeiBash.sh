7047447a7fceb242b6660b14a18daff4a20f087f
Louis Kottmann <louis.kottmann@gmail.com>
2015-10-06 19:37:32 +0200

    fix obsolete style

--- GitRepos/Muzei-Bash/MuzeiBash.sh
+++ GitRepos/Muzei-Bash/MuzeiBash.sh
@@ -2,15 +2,15 @@
 
 ######Initial stuff######
 muzeiDir=~/Pictures/Muzei
-mkdir -p $muzeiDir/Wallpaper
-cd $muzeiDir
+mkdir -p "$muzeiDir"/Wallpaper
+cd "$muzeiDir"
 
 ######Functions##########
 function setWallpaperLinux(){
     if [ "$(which gsettings)" ]
     then
         echo "Gnome-settings-daemons detected, setting wallpaper with gsettings..."
-        gsettings set org.gnome.desktop.background picture-uri file://$muzeiDir/Wallpaper/$imageFile
+        gsettings set org.gnome.desktop.background picture-uri file://"$muzeiDir"/Wallpaper/"$imageFile"
     else
         echo "Salvage your ass, get Linux."
         exit
@@ -36,35 +36,35 @@ fi
 
 ######Get the Muzei JSON and parse it######
 curl -s -o muzei.json 'https://muzeiapi.appspot.com/featured?cachebust=1'
-imageUri=`jq '.imageUri' $muzeiDir/muzei.json | sed s/\"//g`
-imageFile=`basename $imageUri`
-title=`jq '.title' $muzeiDir/muzei.json | sed s/\"//g`
-byline=`jq '.byline' $muzeiDir/muzei.json | sed s/\"//g`
+imageUri="$(jq '.imageUri' $muzeiDir/muzei.json | sed s/\"//g)"
+imageFile="$(basename "$imageUri")"
+title="$(jq '.title' $muzeiDir/muzei.json | sed s/\"//g)"
+byline="$(jq '.byline' $muzeiDir/muzei.json | sed s/\"//g)"
 
 ######Get the latest wallpaper######
 cd Wallpaper
-if [ -f $imageFile ]
+if [ -f "$imageFile" ]
 then
   echo "File $imageFile exists."
 else
   echo "File $imageFile does not exist, downloading..."
-  curl -s -O $imageUri
+  curl -s -O "$imageUri"
 fi
 
 ######Set the wallpaper######
 setWallpaperLinux
 
 ######Send a notification######
-cd $muzeiDir
-if [ -f MuzeiLogo.png ];
+cd "$muzeiDir"
+if [ -f "$muzeiDir"/MuzeiLogo.png ];
 then
   echo "Logo already exists."
 else
   echo "Logo doesn't exist, downloading..."
-  curl -s -O "https://raw.github.com/Feminist-Software-Foundation/Muzei-Bash/master/MuzeiLogo.png"
+  curl -s -o "$muzeiDir"/MuzeiLogo.png "https://raw.github.com/Feminist-Software-Foundation/Muzei-Bash/master/MuzeiLogo.png"
 fi
-notify-send "New wallpaper: '$title'" "$byline" -i $muzeiDir/MuzeiLogo.png
+notify-send "New wallpaper: '$title'" "$byline" -i "$muzeiDir"/MuzeiLogo.png
 
 ######Clean up old wallpapers######
 echo "Cleaning up old files..."
-find $muzeiDir/Wallpaper -ctime +30 -exec rm {} +
+find "$muzeiDir"/Wallpaper -ctime +30 -exec rm {} +
